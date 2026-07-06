2978bbc9d7a55febf343d4873b9b9a4684ca5b89
Pier Giuseppe Meo <piergiuseppe.meo@icar.cnr.it>
2026-07-06 16:40:38 +0200

    manuale

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ Route::redirect('/', '/admin');
 // La rotta riceve l'ID del soggetto (es: l'agente) e il token di sicurezza nei parametri
 Route::get('/bpm-landing/{subject_id}', [BpmBridgeController::class, 'handle'])
     ->name('bpm.landing');
+
+Route::get('/admin/manuale', function () {
+    return response()->download(public_path('docs/Manuale Proforma.pdf'), 'Manuale Proforma.pdf');
+})->name('filament.admin.pages.manuale');
+
+
