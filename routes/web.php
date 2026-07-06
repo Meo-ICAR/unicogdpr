8b89436998ea74f68b3915f5846ed43d01b6fe42
Pier Giuseppe Meo <piergiuseppe.meo@icar.cnr.it>
2026-07-06 16:35:12 +0200

    bpm

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BpmBridgeController;
+use App\Models\Document;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
+
+// La rotta riceve l'ID del soggetto (es: l'agente) e il token di sicurezza nei parametri
+Route::get('/bpm-landing/{subject_id}', [BpmBridgeController::class, 'handle'])
+    ->name('bpm.landing');
